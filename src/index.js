095b75ceb45ba027b134c55d27d968f6c615afc9
dinhvaren <hqhuyzzzz@gmail.com>
2025-09-29 20:56:53 +0700

    deploy

--- src/index.js
+++ src/index.js
@@ -10,13 +10,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/image", express.static(path.join(__dirname, "..", "image")));
-app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "..", "views")));
+// rất quan trọng khi deploy
+app.set("trust proxy", 1);
 app.use(
   session({
     secret: process.env.SESSION_SECRET || "secret-dev",
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: process.env.NODE_ENV === "production" },
+    cookie: {
+      secure: process.env.NODE_ENV === "production", // chỉ bật khi chạy HTTPS
+      sameSite: "lax",
+    },
   })
 );
 
@@ -25,11 +30,8 @@ const host = process.env.HOST;
 
 db.connect();
 
-// rất quan trọng khi deploy
-app.set("trust proxy", 1);
-
 route(app);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://${host}:${port}`);
 });
